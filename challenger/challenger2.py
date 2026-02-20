cfcd22b3bb5cca08bda22600bb5f407b8fde9ad6
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-20 19:55:20 -0300

    Introdução do segundo

---
+++ challenger/challenger2.py
@@ -0,0 +1,7 @@
+'''
+Ei, você consegue me ajudar a mover essas caixas lá para
+fora hoje a tarde?
+
+Se eu estiver livre, sim. Mas se não der, pede meu
+irmão para te ajudaer
+'''
--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
 print("Olá, Mundo!")
+
+
+idade = int(input("\nQual a sua idade? "))
+
+if idade >= 18:
+    print("Você é adulto")
+elif idade >= 12:
+    print("Você é adolescente")
+else:
+    print("Você é criança")

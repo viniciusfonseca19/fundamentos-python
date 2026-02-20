03ab9562a41f687f8a132d580141ccfda30470ad
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-20 19:33:41 -0300

    Primeira challenge

---
+++ challenger/challenger1.py
@@ -0,0 +1,25 @@
+'''
+
+----METODO DOS 5QS----
+
+1. Quais são os dados de entradas necessários
+- Salário Mensal
+- Quantidade de horas trabalhadas
+
+2. O que devo fazer com estes dados?
+- Calcular o valor da hora
+
+3. Quais são as restrições deste problema?
+- Precisa ter um valor do salário mensal
+- Precisa ter um valor da quantidade de horas trabalhadas
+
+4. Qual é o resultado esperado?
+- Exibir o valor da hora da pessoa, com base no cálculo de valor hora
+
+5. Qual é a sequência de passos a ser feita para chegar nesse resultado?
+(pseudocodigo)
+- Reeceber o salário mensal
+- Receber o quantidade de hora trabalhadas
+- Valor hora = salário mensal / quantidade de horas trabalhadas
+- Exibir valor hora
+'''
